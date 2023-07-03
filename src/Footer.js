bf692b0e559d190980ec4add8318b5c496c33076
Name <you@gmail.com>
2023-07-03 20:34:29 +0500

    task couter has been added in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,29 @@
+import { useState, useEffect, Component } from 'react';
+
+
 export default function Footer ( {prop}){
+  const [tasksForFooter, setTasksForFooter]=useState([]); 
 
   
+  useEffect(() => {
+  
+    const items = JSON.parse(localStorage.getItem('сomponents'));
+    if (items) {
+      
+      setTasksForFooter(items);
+    }
+    
+  }, []);
+
+
     return(
     <>
     
       
      
-      <div>{`Active tasks:< ${prop[1]=== undefined || prop[2]=== undefined ? "NO": prop[1].length + prop[2].length}> Finished tasks: <${prop[3]=== undefined? "NO": prop[3].length}>  Kanban board by <NAME>, <YEAR>`} </div>
+      <div>{`Active tasks:< ${(Number.isNaN(prop[0].length)?  0 : prop[0].length )+ (Number.isNaN(prop[1].length)?  0 : prop[1].length ) + (Number.isNaN(prop[2].length)?  0 : prop[2].length )}> Finished tasks: <${(Number.isNaN(prop[3].length)?  0 : prop[3].length )}>  Kanban board by <NAME>, <YEAR>`} </div>
     </>
+    
     )
+    
     }
